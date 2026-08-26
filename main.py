c7278b942ebc0ba90071668f73e3fd4cc0ef217f
cesar <cesar@DESKTOP-1TN96GE>
2026-08-25 22:21:25 -0600

    First routes for the API

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 # Como primera instancia instalar flask
 # pip install flask
 
+from flask import Flask
+
+app = Flask(__name__)
+
+jugadores = [
+        {"id": 1, "nombre": "Duri"},
+        {"id": 2, "nombre": "Cesar"},
+        {"id": 3, "nombre": "Diego"},
+        {"id": 4, "nombre": "Ana"},
+    ]
+
+@app.route('/api/usuarios', methods=['GET'])
+def obtener_jugadores():
+    return {"jugadores": jugadores}
+
+@app.route('/api/usuarios/<int:id>', methods=['GET'])
+def obtener_jugador_por_id(id):
+   for jugador in jugadores:
+        if jugador["id"] == id:
+            return jugador
+
+@app.route('/api/usuarios/<nombre>', methods=['GET'])
+def obtener_jugador_por_nombre(nombre):
+    for jugador in jugadores:
+        if jugador["nombre"] == nombre:
+            return jugador
